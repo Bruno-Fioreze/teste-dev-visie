945f03cf7b5e1de9492f755fa69c9c1d625b8c9e
= <=>
2022-01-19 18:54:36 -0300

    Migrando para o banco oficial e fazendo alguns ajustes.

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -20,9 +20,13 @@ const create_row = (pessoa) => {
     tr.setAttribute("id", `pessoa_${pessoa.id_pessoa}`)
 
     let td_nome = document.createElement("td")
-    td_nome.innerText = pessoa.nome
+    td_nome.setAttribute("id", `pessoa_nome_${pessoa.id_pessoa}`)
+
+    let array_nome = pessoa.nome.split(" ")
+    td_nome.innerText = array_nome[0]
 
     let td_admissao = document.createElement("td")
+    td_admissao.setAttribute("id", `pessoa_dt_admissao_${pessoa.id_pessoa}`)
     td_admissao.innerText =  formata_data( pessoa.data_admissao)
 
     let td_editar = document.createElement("td")
@@ -64,6 +68,13 @@ const set_border = (elemento) => {
     setTimeout( () => {
         elemento.classList.remove("border-danger", "remove_azul")
     }, 3000)
+}
+
+const set_text = (dados, pk) => {
+    const array_nome = dados["nome"].split(" ")
+    const nome = array_nome[0]
+    document.getElementById(`pessoa_nome_${pk}`).innerText = nome
+    document.getElementById(`pessoa_dt_admissao_${pk}`).innerText = formata_data( dados["data_admissao"] )
 }
 
 //requests
@@ -169,7 +180,9 @@ const cadastrar = async () => {
 
     const validator =  validation()
     const form_valid =  verify_fields_form()
-    if( validator.pk  != "" || verify_fields_form() == false  ){
+
+
+    if( validator.pk  != "" || form_valid == false  ){
         swal.close()
         get_swal_alert(["Atenção !", "Revise o formulário.", "warning"])
         return
@@ -216,10 +229,9 @@ const cadastrar = async () => {
 const atualizar = async () => {
 
     const validator = validation()
-    const pk = document.getElementById("pessoa_id").value
+  
     
-    
-    if( pk == "" || verify_fields_form() == false ){
+    if( validator.pk == "" || verify_fields_form() == false ){
         swal.close()
         console.log("nao passou")
         get_swal_alert(["Atenção !", "Revise o formulário.", "warning"])
@@ -240,7 +252,7 @@ const atualizar = async () => {
     };
 
     const location = `${window.location.protocol}${window.location.host}`
-    const url = `${location}/pessoas/${pk}/`
+    const url = `${location}/pessoas/${validator.pk}/`
 
     alerta_carregando()
     const response_code = {
@@ -260,6 +272,7 @@ const atualizar = async () => {
             return
         }
         document.getElementById("pessoa_id").value = validator.pk
+        set_text( dados, validator.pk )
         response_code[response.status](dados, set_data)
     } catch (e){
         console.log(e)
@@ -331,7 +344,7 @@ const verify_fields_form = () => {
     let valid = true
     const form = document.getElementById('form-cadastro').querySelectorAll("input")
     for ( input of form ){
-        if(input.value == ""){
+        if(input.value == "" && input.id != "pessoa_id"){
             set_border(input)
             valid = false
         }
